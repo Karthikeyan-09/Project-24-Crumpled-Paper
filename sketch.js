41f375eb4d100954ba1e0a9568ebc282afb8d71e
Karthikeyan-09 <75008433+Karthikeyan-09@users.noreply.github.com>
2021-01-24 17:39:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var block1, block2, block3;
+var ground, crumpledpaper,paperobject;
+
+
+function setup() {
+	createCanvas(1200, 500);
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	block1 = new Block(890,420,20,100);
+	block2 = new Block(1000,460,200,20);
+	block3 = new Block(1110,420,20,100);
+	ground = new Ground(width/2,480,width,20);
+
+	paperobject = new Paper(200,450,20);
+	
+
+  
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+  ground.display();
+  block1.display();
+  block2.display();
+  block3.display();
+  paperobject.display();
+}
+
+function keyPressed() {	
+	if (keyCode === UP_ARROW) {
+		Body.applyForce(paperobject.paperObject,paperobject.paperObject.position,{x:60,y:-90});
+	}
+}
